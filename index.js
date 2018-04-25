8e459582e94e86e189ca24197872c7878464b76f
Alexandre Rivest <tiaaaa123@users.noreply.github.com>
2018-04-24 21:44:24 -0400

    trying to add horizontal scrolling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {View, PanResponder} from 'react-native';
+import {PanResponder, ScrollView} from 'react-native';
 
 export const swipeDirections = {
   SWIPE_UP: 'SWIPE_UP',
@@ -102,7 +102,7 @@ class GestureRecognizer extends Component {
   }
 
   render() {
-    return (<View {...this.props} {...this._panResponder.panHandlers}/>);
+    return (<ScrollView {...this.props} {...this._panResponder.panHandlers}/>);
   }
 };
 
